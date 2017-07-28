77fa5cc1ba9826cd98dbedb4815bf1ae4833b9e7
Nam Nguyen <nguyenngochainam92@gmail.com>
2017-07-28 19:03:24 +0700

    create db catagories

---
+++ database/migrations/2017_07_28_082803_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('categories', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('image',64)->nullable();
+                $tablle->integer('sort_order',3)->default(0);
+                $table->timestamps();
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
